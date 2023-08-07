6f34c1c6a761b529b5ce4f68b92b2d6bad0533e9
rajkumar31012 <112336458+rajkumar31012@users.noreply.github.com>
2023-08-07 07:23:32 +0530

    logo change in panel

--- resources/views/icm/partials/header.blade.php
+++ resources/views/icm/partials/header.blade.php
@@ -2,7 +2,6 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
    <!-- Brand Logo -->
    <a href="#" class="brand-link">
-     <img src="/panel/dist/img/AdminLTELogo.png" alt="ICM" class="brand-image img-circle elevation-3" style="opacity: .8">
      <span class="brand-text font-weight-light">ICM Panel</span>
    </a>
 
@@ -11,7 +10,7 @@
      <!-- Sidebar user panel (optional) -->
      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
        <div class="image">
-         <img src="/panel/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+         <img src="{{asset('images/tncu-logo.png')}}" class="img-circle elevation-2" alt="User Image">
        </div>
        <div class="info">
          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
