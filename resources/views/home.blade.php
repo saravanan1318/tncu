0fbd71d7071731714880ac7e6154b5d75e092bab
Trophysystems <129720010+Trophysystems@users.noreply.github.com>
2023-09-01 17:18:29 +0530

    Daniel Form Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                    <ul class="list-aggregate" id="">
                       <li>
                          <a href="/applicationform" style="color: darkred;">
-                           Diplomo Course Online Application
+                           Diploma Course Online Application
                             <img src="{{asset('images/new.gif')}}" alt="Blink New">
                          </a>
                       </li>
