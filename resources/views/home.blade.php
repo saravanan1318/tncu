e0d58ade3621b6e96cf0541ef23070d51d67e859
Trophysystems <129720010+Trophysystems@users.noreply.github.com>
2023-09-02 14:50:19 +0530

    Pagarap Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                    <ul class="list-aggregate" id="">
                       <li>
                          <a href="/applicationform" style="color: darkred;">
-                           Diploma Course Online Application
+                           Diploma Course Online Application 2023 - 2024
                             <img src="{{asset('images/new.gif')}}" alt="Blink New">
                          </a>
                       </li>
