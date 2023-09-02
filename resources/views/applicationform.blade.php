882150c8ab4cd61657de64f91596ff53220ed8da
Trophysystems <129720010+Trophysystems@users.noreply.github.com>
2023-09-02 13:50:12 +0530

    Pagarap Update

--- resources/views/applicationform.blade.php
+++ resources/views/applicationform.blade.php
@@ -165,7 +165,7 @@
                     <hr>
                     <div class="row">
                         <div class="col-sm-12 subHeadings">
-                            <b>{{__('form.Paddress')}}</b>
+                            <b>{{__('form.address')}}</b>
                         </div>
                     </div>
                     <div class="clearfix"></div>
@@ -243,7 +243,7 @@
                     <hr>
                     <div class="row">
                         <div class="col-sm-12 subHeadings">
-                            <b> {{__('form.address')}}</b>
+                            <b> {{__('form.Paddress')}}</b>
                         </div>
                     </div>
                     <div class="clearfix"></div>
