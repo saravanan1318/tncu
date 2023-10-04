7b139cfcaf681a961a168b98e9ebd3bf97c15e4c
rajkumar31012 <112336458+rajkumar31012@users.noreply.github.com>
2023-10-04 07:40:51 +0530

    print invoice chnages

--- resources/views/icm/printinvoice.blade.php
+++ resources/views/icm/printinvoice.blade.php
@@ -81,11 +81,11 @@
     <h2 class="text-center m-0 p-0">Tamil Nadu Cooperative Union</h2>
 </div>
 <div class="head-title">
-    <h2 class="text-center m-0 p-0">Invoice</h2>
+    <h2 class="text-center m-0 p-0">INVOICE</h2>
 </div>
 <div class="add-detail mt-10">
     <div class="w-50 float-left mt-10">
-        <p class="m-0 pt-5 text-bold w-100">Invoice Id - <span class="gray-color">{{$invoicedetails[0]->invoiceNo}}</span></p>
+        <p class="m-0 pt-5 text-bold w-100">Invoice No. - <span class="gray-color">{{$invoicedetails[0]->invoiceNo}}</span></p>
         <p class="m-0 pt-5 text-bold w-100">Invoice Date - <span class="gray-color">{{date("d-m-Y",strtotime($invoicedetails[0]->created_at))}}</span></p>
     </div>
     <div class="w-50 float-left logo mt-10">
@@ -102,17 +102,17 @@
         <tr>
             <td>
                 <div class="box-text">
-                    <p>{{ Auth::user()->name }}</p>
-                    <p>{{ $icm->add1.", ".$icm->add2 }}</p>
-                    <p>{{ $icm->city.", ".$icm->pincode }}</p>                    
+                    <p><b>{{  $icm->icm_name }}</b></p>
+                    <p style="line-height: 1.6;">{{ $icm->add1.", ".$icm->add2 }}</p>
+                    <p style="line-height: 1.6;">{{ $icm->city.", ".$icm->pincode }}</p>                    
                     <p>Contact: {{ $icm->contact }}</p>
                 </div>
             </td>
             <td>
                 <div class="box-text">
-                    <p>{{$studentData->fullname}}</p>
-                    <p>{{$studentData->plotno.", ".$studentData->streetname.", ".$studentData->city}}</p>
-                    <p>{{$studentData->district.", ".$studentData->state.", ".$studentData->pincode}}</p>            
+                    <p><b>{{$studentData->fullname}}</b></p>
+                    <p style="line-height: 1.6;">{{$studentData->plotno.", ".$studentData->streetname.", ".$studentData->city}}</p>
+                    <p style="line-height: 1.6;">{{$studentData->district.", ".$studentData->state.", ".$studentData->pincode}}</p>            
                     <p>Contact: {{$studentData->mobile1}}</p>
                 </div>
             </td>
@@ -143,15 +143,56 @@
                 01
                 </td>
                 <td style="text-align: center">
-                    {{$invoicedetail->amount}}
+                    {{number_format((float)$invoicedetail->amount, 2, '.', '');}}
                 </td>
                 <td style="text-align: center">
-                    {{$invoicedetail->amount}}
+                    {{number_format((float)$invoicedetail->amount, 2, '.', '');}}
                 </td>
             </tr>
         <?php
           $total += $invoicedetail->amount;
         }
+
+        $number = $total;
+        $no = floor($number);
+        $point = round($number - $no, 2) * 100;
+        $hundred = null;
+        $digits_1 = strlen($no);
+        $i = 0;
+        $str = array();
+        $words = array('0' => '', '1' => 'one', '2' => 'two',
+            '3' => 'three', '4' => 'four', '5' => 'five', '6' => 'six',
+            '7' => 'seven', '8' => 'eight', '9' => 'nine',
+            '10' => 'ten', '11' => 'eleven', '12' => 'twelve',
+            '13' => 'thirteen', '14' => 'fourteen',
+            '15' => 'fifteen', '16' => 'sixteen', '17' => 'seventeen',
+            '18' => 'eighteen', '19' =>'nineteen', '20' => 'twenty',
+            '30' => 'thirty', '40' => 'forty', '50' => 'fifty',
+            '60' => 'sixty', '70' => 'seventy',
+            '80' => 'eighty', '90' => 'ninety');
+        $digits = array('', 'hundred', 'thousand', 'lakh', 'crore');
+        while ($i < $digits_1) {
+            $divider = ($i == 2) ? 10 : 100;
+            $number = floor($no % $divider);
+            $no = floor($no / $divider);
+            $i += ($divider == 10) ? 1 : 2;
+            if ($number) {
+                $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+                $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+                $str [] = ($number < 21) ? $words[$number] .
+                    " " . $digits[$counter] . $plural . " " . $hundred
+                    :
+                    $words[floor($number / 10) * 10]
+                    . " " . $words[$number % 10] . " "
+                    . $digits[$counter] . $plural . " " . $hundred;
+            } else $str[] = null;
+        }
+        $str = array_reverse($str);
+        $result = implode('', $str);
+        $points = ($point) ?
+            "." . $words[$point / 10] . " " . 
+                $words[$point = $point % 10] : '';
+        $amountinrupees = $result . "Rupees Only";
         ?>
         <tr>
             <td colspan="5">
@@ -161,8 +202,8 @@
                         <p>Total Payable</p>
                     </div>
                     <div class="total-right w-15 float-left text-bold" align="right">
-                        <p>{{$total}}</p>
-                        <p>{{$total}}</p>
+                        <p>{{number_format((float)$total, 2, '.', '');}}</p>
+                        <p>{{number_format((float)$total, 2, '.', '');}}</p>
                     </div>
                     <div style="clear: both;"></div>
                 </div> 
@@ -170,6 +211,12 @@
         </tr>
     </table>
 </div>
+<div class="add-detail mt-10" style="font-size: 12px !important">
+    <div class="w-100 float-left mt-10">
+       <b> Amount in words: </b> {{ucfirst($amountinrupees)}}
+    </div>
+</div>
+<br>
 <div class="table-section bill-tbl w-100 mt-10">
     <table class="table w-100 mt-10">
         <tr>
@@ -181,5 +228,11 @@
             {{-- <td>Free Shipping - Free Shipping</td> --}}
         </tr>
     </table>
+</div>
+<div class="add-detail mt-10" style="font-size: 10px !important">
+    <div class="w-50 float-left mt-10">
+        Terms & Conditions:
+        <p>1. Once amount paid cannot refundable.</p>
+    </div>
 </div>
 </html>
